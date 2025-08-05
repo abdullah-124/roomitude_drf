924fad3a10f4fdacea7cfe5a66313fb99ef3175b
abdullah-124 <mdabdullahsakib468@gmail.com>
2025-08-05 17:12:43 +0600

    pagination added

--- roomitude_backend/settings.py
+++ roomitude_backend/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
 ]
 # restframework settings 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12,  # Number of items per page
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',  # for browsable API
         'rest_framework_simplejwt.authentication.JWTAuthentication',  # for API clients
